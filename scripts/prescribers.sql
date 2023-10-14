fb63d95946a79d4c80a15b547b99c04021660104
caleb.lucas <cplucas1133@gmail.com>
2023-10-14 10:29:08 -0500

    cont. on 7

--- scripts/prescribers.sql
+++ scripts/prescribers.sql
@@ -2,8 +2,8 @@
 --     a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
 SELECT npi, SUM(total_claim_count) AS total_claims
-FROM prescriber AS p1
-LEFT JOIN prescription AS p2
+FROM prescriber 
+LEFT JOIN prescription 
 USING (npi)
 WHERE total_claim_count IS NOT NULL
 GROUP BY npi
@@ -31,7 +31,7 @@ FROM prescriber p1
 LEFT JOIN prescription p2
 ON p1.npi = p2.npi
 GROUP BY specialty_description
-ORDER BY COUNT (total_claim_count) DESC;
+ORDER BY SUM(total_claim_count) DESC;
 
 -- Answer: Nurse Practitioner, 7185315
 
@@ -56,7 +56,7 @@ ORDER BY COUNT (total_claim_count) DESC;
 -- 3. 
 --     a. Which drug (generic_name) had the highest total drug cost?
 
-SELECT drug_name, generic_name, total_drug_cost
+SELECT drug_name, generic_name, CAST(total_drug_cost AS money)
 FROM prescription 
 LEFT JOIN drug 
 USING (drug_name)
@@ -66,11 +66,13 @@ ORDER BY total_drug_cost DESC;
 
 --     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
 
-SELECT drug_name, generic_name, total_day_supply, total_30_day_fill_count, total_drug_cost
-FROM prescription p
-LEFT JOIN drug d
+SELECT generic_name, CAST(ROUND((SUM(total_drug_cost) / SUM(total_day_supply)),2)AS MONEY) as cost_per_day
+FROM drug
+FULL JOIN prescription
 USING (drug_name)
-ORDER BY total_drug_cost DESC;
+WHERE total_drug_cost IS NOT NULL
+GROUP BY generic_name
+ORDER BY cost_per_day DESC;
 
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
@@ -83,10 +85,23 @@ FROM drug;
 
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
+SELECT
+CASE
+WHEN d.opioid_drug_flag = 'Y' THEN 'opioid'
+WHEN d.antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+ELSE 'neither'
+END AS drug_type,
+CAST(SUM(p.total_drug_cost) AS money) AS total_spent
+FROM drug AS d
+LEFT JOIN prescription AS p
+USING(drug_name)
+GROUP BY drug_type
+ORDER BY total_spent DESC;
+
 -- 5. 
 --     a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
 
-SELECT cbsaname
+SELECT cbsa, cbsaname
 FROM cbsa
 WHERE cbsaname LIKE '%TN%'
 GROUP BY cbsaname, cbsa;
